4645a43ef9c8d844a5e329ba8910cde93c57cdfd
Ilya Skorokhodov <skorox2000@gmail.com>
2023-01-16 11:02:23 +0300

    Change theme of shop to car details shop
    Change: models; data for default db; programm logic.

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
+    type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
 
 class Purchase(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+
+class ProductToPurchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    finalPrice = models.PositiveIntegerField()
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,10 @@
+import copy
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 
-from .models import Product, Purchase
+from .models import Product, Purchase, ProductToPurchase
 
 # Create your views here.
 def index(request):
@@ -13,9 +15,53 @@ def index(request):
 
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['product', 'person', 'address']
+    fields = ['person', 'address']
 
     def form_valid(self, form):
         self.object = form.save()
+        allProducts = Product.objects.all()
+        requestBuyList = self.request.POST['products'].split(",")
+        finalBuyList = []
+        #print(len(requestBuyList))
+        #print("------------------------------")
+        for pi in range(len(requestBuyList)):
+            for p in allProducts:
+                if str(p.id) == str(requestBuyList[pi]):
+                    finalBuyList.append(copy.deepcopy(p))
+
+        #print(finalBuyList)
+
+        buyListTypes = []
+        buyListTypesCount = []
+
+        for product in finalBuyList:
+            j = -1
+            for idType, line in enumerate(buyListTypes):
+                if str(line) == str(product.type.id):
+                    j = idType
+                    break;
+
+            if j != -1:
+                buyListTypesCount[j] = buyListTypesCount[j]+1
+            else:
+                buyListTypes.append(product.type.id)
+                buyListTypesCount.append(1)
+
+        items_with_discount = 0
+
+        if len(buyListTypesCount)>2:
+            buyListTypesCount.sort()
+            items_with_discount = buyListTypesCount[0]
+
+            finalBuyList.sort(key=lambda x: x.price)
+
+
+        for idproduct, product in enumerate(finalBuyList):
+            if items_with_discount > 0 and idproduct < items_with_discount:
+                product.price = product.price/10*6
+
+            #print(product.price)
+            ProductToPurchase.objects.create(product=Product.objects.get(pk=product.id), purchase=self.object, finalPrice=product.price)
+
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
 
